c4247e3b433f6b086bd80efcbeff240b45f2cc54
HappyClass-ops <goldenhappyaku@gmail.com>
2026-05-04 09:53:19 +0100

    UI_5

--- scripts/assets.js
+++ scripts/assets.js
@@ -9,12 +9,12 @@ window.ROOT_ASSETS = {
     // -----------------------------------------------------------------
     // BACKGROUND MUSIC
     // -----------------------------------------------------------------
-    // Paste a URL here OR just put the filename like 'assets/menu_music.mp3'
-    SoundOfArcadeMenu: "https://media.vocaroo.com/mp3/15gsH2QdDGX5",
+    // This points the game directly to the MP3 you just added!
+    // (Make sure the capital M's match your exact file name)
+    SoundOfArcadeMenu: "assets/Menu_Music.mp3",
 
     // -----------------------------------------------------------------
     // IMAGES & LOGOS
     // -----------------------------------------------------------------
-    // A fallback invisible pixel so the menu doesn't break if a link dies
     ImageSafetyNet: "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png"
 };
